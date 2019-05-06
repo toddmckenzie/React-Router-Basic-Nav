851f02155947d5e21c5bcb9c57115334b35a2096
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-05-06 15:02:57 -0500

    switched

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,24 +5,24 @@ const Navigation = () => {
   return (
     <div>
       <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <Link to='/'>
-            <a href="">Home</a>
-          </Link>
-        </div>
-        <div>
-        <Link to='/about'>
-          <a href="">About</a>
-        </Link>
-        </div>
-        <div>
-        <Link to='/contact'>
-          <a href="">Contact</a>
-        </Link>
+          <h1>React Router Mini</h1>
+          <div>
+            <Link to='/'>
+              <a href='#'>Home</a>
+            </Link>
+          </div>
+          <div>
+            <Link to='/about'>
+              <a href='#'>About</a>
+            </Link>
+          </div>
+          <div>
+            <Link to='/contact'>
+              <a href='#'>Contact</a>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
